179b0499402f5f60ee4fc9589721130f4f64bf9b
Kuba Nowak <kubanowak9755@gmail.com>
2020-12-10 08:14:28 +0100

    Used sidekiq gem to process movie operations asynchronously

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,14 +10,12 @@ class MoviesController < ApplicationController
   end
 
   def send_info
-    @movie = Movie.find(params[:id])
-    MovieInfoMailer.send_info(current_user, @movie).deliver_now
+    MovieInfoSenderWorker.perform_async(params[:id], current_user.id)
     redirect_back(fallback_location: root_path, notice: "Email sent with movie info")
   end
 
   def export
-    file_path = "tmp/movies.csv"
-    MovieExporter.new.call(current_user, file_path)
+    MoviesCsvExportWorker.perform_async(current_user.id)
     redirect_to root_path, notice: "Movies exported"
   end
 end
